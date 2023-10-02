b0c02f6857cd16c349fc55b0af856dc5a71eafe6
Mark Cullen <mark.cullen@satalia.com>
2023-10-02 15:50:31 +0100

    Role policy 2

--- main.tf
+++ main.tf
@@ -1,12 +1,66 @@
+# Define the provider (AWS in this case)
 provider "aws" {
-  region = var.region
+  region = "eu-east-2" # Change to your desired AWS region
 }
 
+# Create an IAM role for the Lambda function
+resource "aws_iam_role" "lambda_role" {
+  name = "my-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Create an IAM policy for the Lambda function
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "LambdaPolicy"
+  description = "IAM policy for Lambda function"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = "logs:CreateLogGroup",
+        Effect   = "Allow",
+        Resource = "*",
+      },
+      {
+        Action   = "logs:CreateLogStream",
+        Effect   = "Allow",
+        Resource = "*",
+      },
+      {
+        Action   = "logs:PutLogEvents",
+        Effect   = "Allow",
+        Resource = "*",
+      },
+    ],
+  })
+}
+
+# Attach the Lambda policy to the Lambda role
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  policy_arn = aws_iam_policy.lambda_policy.arn
+  role       = aws_iam_role.lambda_role.name
+}
+
+# Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
+  bucket = "my-unique-bucket-name"
   acl    = "private" # You can adjust the access control as needed
 }
 
+# Create the Lambda function
 resource "aws_lambda_function" "my_lambda" {
   filename      = "lambda_function.zip" # Path to your Lambda deployment package
   function_name = "my-lambda-function"
@@ -22,57 +76,23 @@ resource "aws_lambda_function" "my_lambda" {
       key2 = "value2",
     }
   }
+
+  # Optional: Define the S3 bucket trigger (uncomment if needed)
+  # event_source {
+  #   s3 {
+  #     bucket = aws_s3_bucket.my_bucket.id
+  #     events = ["s3:ObjectCreated:*"]
+  #     filter_prefix = "my-folder/"
+  #   }
+  # }
 }
 
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "LambdaPolicy"
-  description = "IAM policy for Lambda function"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "lambda:CreateFunction",
-          "lambda:InvokeFunction",
-          "lambda:DeleteFunction",
-          "lambda:GetFunction",
-          "lambda:UpdateFunctionConfiguration",
-        ],
-        Effect   = "Allow",
-        Resource = "*",
-      },
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-        ],
-        Effect   = "Allow",
-        Resource = "*",
-      },
-    ],
-  })
+# Output the ARN of the Lambda function for reference
+output "lambda_function_arn" {
+  value = aws_lambda_function.my_lambda.arn
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "my-lambda-role"
-
-  trust_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  policy_arn = aws_iam_policy.lambda_policy.arn
-  role       = aws_iam_role.lambda_role.name
+# Output the name of the S3 bucket for reference
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
 }
