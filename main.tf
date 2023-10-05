c86b40aa3f6e8a0d6e0a4d9fabc03076ce7400a1
Mark Cullen <mark.cullen@satalia.com>
2023-10-05 22:49:01 +0100

    AppSync added 3

--- main.tf
+++ main.tf
@@ -135,3 +135,17 @@ resource "aws_lambda_function" "my_lambda" {
 }
 
 
+# Output the GraphQL API endpoint
+output "appsync_api_endpoint" {
+  value = aws_appsync_graphql_api.my_appsync_api.uris[0]
+}
+
+# Output the ARN of the Lambda function for reference
+output "lambda_function_arn" {
+  value = aws_lambda_function.my_lambda.arn
+}
+
+# Output the name of the S3 bucket for reference
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
+}
--- outputs.tf
+++ outputs.tf
@@ -6,17 +6,3 @@
 #   value = aws_instance.ubuntu.arn
 # }
 
-# Output the GraphQL API endpoint
-output "appsync_api_endpoint" {
-  value = aws_appsync_graphql_api.my_appsync_api.uris[0]
-}
-
-# Output the ARN of the Lambda function for reference
-output "lambda_function_arn" {
-  value = aws_lambda_function.my_lambda.arn
-}
-
-# Output the name of the S3 bucket for reference
-output "s3_bucket_name" {
-  value = aws_s3_bucket.my_bucket.id
-}
