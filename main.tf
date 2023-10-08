11292fdb5aa3b1843c19ba0c99e6661677f93681
Mark Cullen <mark.cullen@satalia.com>
2023-10-08 22:46:26 +0100

    Seed Cognito 3

--- main.tf
+++ main.tf
@@ -198,7 +198,7 @@ resource "aws_lambda_function" "seed_cognito" {
 
   environment {
     variables = {
-      COGNITO_USER_POOL_ID = module.cognito.cognito_user_pool_id # Use the User Pool ID from the module output
+      COGNITO_USER_POOL_ID = aws_cognito_user_pool.my_user_pool.id # Use the User Pool ID from the module output
     }
   }
 }
